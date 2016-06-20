a19bcd8086f249415006ac95f463559ec6f2dd2b
michaeldelli-gatti <michaelddg@gmail.com>
2016-06-20 12:08:42 -0400

    add post routes

---
+++ src/main/java/com/theironyard/User.java
@@ -0,0 +1,12 @@
+package com.theironyard;
+
+/**
+ * Created by michaeldelli-gatti on 6/20/16.
+ */
+public class User {
+    String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
